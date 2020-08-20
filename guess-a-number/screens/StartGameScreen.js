dc301b5f8ae551aecf9e7329ff0b3fb0055cfc49
nhi-ngo <nhingo.y@gmail.com>
2020-08-20 13:53:01 -0400

    validate user input and control keyboard display

--- guess-a-number/screens/StartGameScreen.js
+++ guess-a-number/screens/StartGameScreen.js
@@ -1,33 +1,50 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Card from '../components/Card';
 import Input from '../components/Input';
 import colors from '../constants/colors';
 
-const StartGameScreen = () => (
-  <View style={styles.screen}>
-    <Text style={styles.title}>Start a New Game!</Text>
-    <Card style={styles.inputContainer}>
-      <Text>Select a Number</Text>
-      <Input
-        style={styles.input}
-        blurOnSubmit // only works in Android
-        autoCapitalize="none"
-        autoCorrect={false}
-        keyboardType="number-pad"
-        maxLength={2}
-      />
-      <View style={styles.buttonContainer}>
-        <View style={styles.button}>
-          <Button title="Reset" onPress={() => {}} color={colors.lightGreen}></Button>
-        </View>
-        <View style={styles.button}>
-          <Button title="Confirm" onPress={() => {}} color={colors.darkGreen}></Button>
-        </View>
+const StartGameScreen = () => {
+  const [enteredValue, setEnteredValue] = useState('');
+
+  // Replace any non-numbers with an empty string
+  const onNumberInput = input => {
+    setEnteredValue(input.replace(/[^0-9]/g, ''));
+  };
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game!</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a Number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit // only works in Android
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="number-pad"
+            maxLength={2}
+            onChangeText={onNumberInput}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button title="Reset" onPress={() => {}} color={colors.lightGreen}></Button>
+            </View>
+            <View style={styles.button}>
+              <Button title="Confirm" onPress={() => {}} color={colors.darkGreen}></Button>
+            </View>
+          </View>
+        </Card>
       </View>
-    </Card>
-  </View>
-);
+    </TouchableWithoutFeedback>
+  );
+};
 
 const styles = StyleSheet.create({
   screen: {
