37928e93a06c66dd83f29cdadab734f5d279b695
nhi-ngo <nhingo.y@gmail.com>
2020-08-24 21:40:52 -0400

    add and style local image

--- guess-a-number/screens/GameOverScreen.js
+++ guess-a-number/screens/GameOverScreen.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, Image } from 'react-native';
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 
 const GameOverScreen = ({ userNumber, roundsNumber, onRestartGame }) => (
   <View style={styles.screen}>
-    <Text>The Game is Over!</Text>
-    <Text>Number of rounds: {roundsNumber} </Text>
-    <Text>Input number was: {userNumber}</Text>
+    <TitleText>The Game is Over!</TitleText>
+    <View style={styles.imageContainer}>
+      <Image
+        source={require('../assets/success.png')}
+        style={styles.image}
+        // resizeMode="content"
+      />
+    </View>
+
+    <BodyText>Number of rounds: {roundsNumber} </BodyText>
+    <BodyText>Input number was: {userNumber}</BodyText>
     <Button title="NEW GAME" onPress={onRestartGame} />
   </View>
 );
@@ -15,6 +25,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: 30,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
 });
 
